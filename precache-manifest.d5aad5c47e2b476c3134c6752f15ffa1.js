1235ede1e9306514b98746c0542f7729f4602c7c
Melody Then <melody.then@mail.mcgill.ca>
2020-08-04 14:03:05 -0400

    Updates

---
+++ precache-manifest.d5aad5c47e2b476c3134c6752f15ffa1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25da3d3cd12a210d83b49e4a002e0cec",
+    "url": "/pomodoroClock/index.html"
+  },
+  {
+    "revision": "62452a4cc185b2daf1ef",
+    "url": "/pomodoroClock/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "52ba015a43b73883137d",
+    "url": "/pomodoroClock/static/css/main.22b1ffd3.chunk.css"
+  },
+  {
+    "revision": "62452a4cc185b2daf1ef",
+    "url": "/pomodoroClock/static/js/2.48c28238.chunk.js"
+  },
+  {
+    "revision": "55fc676fdc7ca9e494c0eed4405b4601",
+    "url": "/pomodoroClock/static/js/2.48c28238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52ba015a43b73883137d",
+    "url": "/pomodoroClock/static/js/main.e6c53eab.chunk.js"
+  },
+  {
+    "revision": "01ad8c670281af1c7683",
+    "url": "/pomodoroClock/static/js/runtime-main.69a22b20.js"
+  }
+]);
